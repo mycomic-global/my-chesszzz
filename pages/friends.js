70db14e4efa9a4c6104973b1e004f60e57bc159d
mycomic-global <sherrinncherryy@gmail.com>
2026-02-25 15:51:46 +0700

    Create friends.js

---
+++ pages/friends.js
@@ -0,0 +1,8 @@
+export default function Friends() {
+  return (
+    <div style={{padding:20}}>
+      <h1>Friends System (Coming Soon)</h1>
+      <p>Add friends, invite match, chat system next update.</p>
+    </div>
+  )
+}
